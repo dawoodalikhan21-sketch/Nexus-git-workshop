560471c680f557105bac5d98e1a2597cc2733f03
dawoodalikhan-21sketch <dawoodalikhan21@gamil.com>
2025-10-16 16:24:11 +0530

    change abdul

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
-
-
 #include <stdio.h>
 int main(){
-    printf("nexus");
+    printf("main dawood")
+    printf("main aman")
     return 0;
 }
--- omer.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("omer");
-    return 0;
-}
